2beabdeda3dbbfa5b3728a62d854d6bf1e93a697
jaypal1184 <jaypalpatel1184@gmail.com>
2025-06-19 12:10:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("uploadBtn").addEventListener("click", () => {
+  alert("Upload feature coming soon!");
+});
+
+document.getElementById("storyBtn").addEventListener("click", () => {
+  alert("Story feature coming soon!");
+});
+
+document.getElementById("profileBtn").addEventListener("click", () => {
+  alert("Profile feature coming soon!");
+});
+
+document.getElementById("cameraBtn").addEventListener("click", () => {
+  alert("Camera feature coming soon!");
+});
